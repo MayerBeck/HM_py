74c2b7887864427422eae856921ff1704cdf5312
Майрбек Хайтаев <mayerbeck83@gmail.com>
2023-01-04 14:12:48 +0300

    finish commit

---
+++ HM_py_1.py
@@ -0,0 +1,42 @@
+# num = int(input('Введите число: ' ''))
+# f_n = 1
+# for i in range(num):
+#     i = i + 1 
+#     f_n = f_n * i
+# print (f'Факториалом числа {num} будет {f_n}')
+
+# print ("Введите число: ")
+# n = int(input())
+# i = 2
+# while i <= n:
+#     if n%i == 0:
+#         print ("Наименьший натуральный делитель числа", n , '=', i)
+#         break
+#     i += 1
+
+# n = int(input('Введите число:'))
+# list=[]                                     
+# for i in range(-n, n+1):    
+#     list.append(i)                     
+# print(list)
+
+# # Создаем список индексов
+# print('Введите количество элементов n:')
+# index = []                                 
+# for i in range(n):                        
+#     n=int(input("Введите число списка индексов: "))
+#     index.append(n)
+# print(index)    
+
+# result = 1
+# for ind in index:
+#     result *= list[ind]
+# print(f'Произведение элементов на введенных индексах = {result}')    
+
+# n = int(input('Введите число:'))   
+# sum = 0                                   
+# for i in range(2,n + 1,2):                        
+#     sum += i  
+# print('Сумма четных чисел в списке: ')
+# print(sum)
+
