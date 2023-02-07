d334efdfd5eaef5f4d19948855bcd68c4482bcf4
Майрбек Хайтаев <mayerbeck83@gmail.com>
2023-02-07 18:03:58 +0300

    finish commit

---
+++ HM_py_6.py
@@ -0,0 +1,22 @@
+# input_nums = [int(i) for i in input("Введите числа через пробел: ").split()]
+# output = filter(lambda x: len(str(abs(int(x)))) == 2, input_nums)
+
+# print("Двузначные числа: ", list(output))
+
+
+# my_list = [12, 'sadf', 5643]
+
+# string = filter(lambda x: not str(x).isdigit(), my_list)
+# digit = filter(lambda x: str(x).isdigit(), my_list)
+
+# print("Строки: ", list(string))
+# print("Числа: ", list(digit))
+
+
+# number = input("Введите любое вещественное число: ")
+
+# list_num = list(map(int, (i for i in number if i.isdigit())))
+
+# sum_digit = sum(list_num)
+
+# print(f"Сумма его цифр = {sum_digit}")
